d1ed6165c5f43c4644c6b16cbe4321bf2fb6aa6e
BonkeGcobo <madixmicro@gmail.com>
2022-05-10 14:44:00 +0200

    generate unique id

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 class Person
   def initialize(age, name = 'unknown', parent_permission: true)
-    @id = 0
+    @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
