b7bfd244b4b4e99ac7aa0d0dbf5782e833711d8f
BonkeGcobo <madixmicro@gmail.com>
2022-05-18 14:31:06 +0200

    add add_rental method

--- book.rb
+++ book.rb
@@ -1,10 +1,17 @@
 require './rental'
+require './person.rb'
 class Book
   attr_accessor :title, :author
+  attr_reader :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
+  end
+
+  def add_rental(person, date)
+    rental = Rental.new(date, self, person)
+    @rentals.push(rental)
   end
 end
