44c5927d257a272c0bde041a2bf8a046d03a684e
BonkeGcobo <madixmicro@gmail.com>
2022-05-18 13:49:55 +0200

    Add person to rental relationship

--- person.rb
+++ person.rb
@@ -1,11 +1,13 @@
 require './nameable'
+require './rental.rb'
 class Person < Nameable
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(age,name = 'unknown', parent_permission: true)
     super()
     @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def find_id
--- rental.rb
+++ rental.rb
@@ -1,8 +1,10 @@
 class Rental
   attr_accessor :date
   attr_accessor :book
-  def initialize (date,book)
+  attr_accessor :person
+  def initialize (date,book, person)
     @date = date
     @book = book
+    @person = person
   end
 end
