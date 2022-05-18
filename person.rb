9db7b6cfbcd9b74f525e845096700d3ed65892ba
BonkeGcobo <madixmicro@gmail.com>
2022-05-18 14:41:36 +0200

    Add accessor for name, age and rentals

--- person.rb
+++ person.rb
@@ -1,6 +1,7 @@
 require './nameable'
 require './rental'
 class Person < Nameable
+  attr_accessor :name, :age, :rentals
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
     @id = Random.rand(1...1000)
