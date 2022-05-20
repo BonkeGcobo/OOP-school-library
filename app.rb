d228f4a474a70604cb7e0f425ef343b3dcd27072
BonkeGcobo <madixmicro@gmail.com>
2022-05-20 13:34:54 +0200

    Fix linter errors

--- app.rb
+++ app.rb
@@ -1,55 +1,48 @@
 require './student'
-require './teacher.rb'
+require './teacher'
 class App
   attr_reader :people
+
   def initialize
     @people = []
   end
 
   def run
-    input = ""
-    while(input!="7")
-      puts "1- List all books"
-      puts "2- List all people"
-      puts "3- Create a person"
-      puts "4- Create a book"
-      puts "5- Create a rental"
-      puts "6- List all rentals for a given id"
-      puts "7- Exit"
+    input = ''
+    while input != '7'
+      puts '1- List all books'
+      puts '2- List all people'
+      puts '3- Create a person'
+      puts '4- Create a book'
+      puts '5- Create a rental'
+      puts '6- List all rentals for a given id'
+      puts '7- Exit'
       input = gets.chomp
-      if(input == "3")
-        create_person()
-      end  
-    end   
+      create_person if input == '3'
+    end
   end
 
   def create_person
-    puts "Do you want to create a student (1) or a teacher (2)? [Input the number]: "
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     num_input = gets.chomp
-    if(num_input == "1")
-      print "Age: "
-      age = gets.chomp
-      print "Name: "
-      name = gets.chomp
-      print "Has parent permission [Y/N]: "
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+    case num_input
+    when '1'
+      print 'Has parent permission [Y/N]: '
       permission_input = gets.chomp.downcase
       permission = permission_input != 'n'
-      person = Student.new(age,nil,name, parent_permission: permission)
+      person = Student.new(age, nil, name, parent_permission: permission)
       @people.push(person)
-    elsif (num_input =="2")
-      print "Age: "
-      age = gets.chomp
-      print "Name: "
-      name = gets.chomp
-      print "Specialization: "
+    when '2'
+      print 'Specialization: '
       special = gets.chomp
-      person = Teacher.new(age,special,name)
+      person = Teacher.new(age, special, name)
       @people.push(person)
-    else
-      puts create_person()
-    end 
-    puts "Person created successfully"
-
+    end
+    puts 'Person created successfully'
   end
 end
 app = App.new
--- student.rb
+++ student.rb
@@ -1,6 +1,7 @@
 require './person'
 class Student < Person
   attr_accessor :classroom
+
   def initialize(age, classroom, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
