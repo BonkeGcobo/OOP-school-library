be17b492dae99f073e4503991781d8b6ce6e8d02
daudi13 <davouma13@gmail.com>
2022-05-24 06:13:45 +0300

    modify main_inputs methods to accomodate user input

--- main_inputs.rb
+++ main_inputs.rb
@@ -1,5 +1,6 @@
 module Inputs
-  def user_input(inputs)
+  def user_input
+    inputs = gets.chomp
     case inputs
     when '1'
       list_books
@@ -14,5 +15,26 @@ module Inputs
     when '6'
       list_rentals
     end
+  end
+
+  def interactions
+    case user_input
+    when '1'
+      list_books
+    when '2'
+      list_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_rentals
+    end
+  end
+
+  def num_input
+
   end
 end
