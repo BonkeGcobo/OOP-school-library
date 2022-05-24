306b830bb162253cbdcb85487dba1f3b578b748c
BonkeGcobo <madixmicro@gmail.com>
2022-05-24 18:22:46 +0200

    Add condition if data is empty

--- data.rb
+++ data.rb
@@ -3,11 +3,16 @@ require 'json'
 
 module Data
   def save_data(list,string_list)
-    file_path = IO.sysopen("./#{string_list}.json",'w+')
-    list_add = IO.new(file_path)
-    list.each{ |data|
-      json = JSON.generate(data)
-      list_add.puts(json)
-    }
+    if(list ==[])
+      puts "No #{string_list} data"
+  
+    else
+      file_path = IO.sysopen("./#{string_list}.json",'w+')
+      list_add = IO.new(file_path)
+      list.each{ |data|
+        json = JSON.generate(data)
+        list_add.puts(json)
+      }
+    end
   end
 end
