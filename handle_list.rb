f00396e7706be6fe22366cc21ea1aef9749f3bc1
daudi13 <davouma13@gmail.com>
2022-05-24 06:45:53 +0300

    Add rental_list in HandleList

--- handle_list.rb
+++ handle_list.rb
@@ -1,19 +1,23 @@
 module HandleList
-  def handle_list(list_items, id = 0)
+  def handle_list(list_items)
     if list_items == @people
       list_items.each_with_index do |person, index|
         puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.find_id}, Age: #{person.age}", "\n"
       end
-    elsif list_items == @books
+    else
       list_items.each_with_index do |book, index|
         print "#{index}) Title: #{book.title} Author: #{book.author}", "\n"
       end
-    else
-      list_items.each do |rental|
+    end
+  end
+
+  def rental_list 
+    id = gets.chomp
+    puts 'Rentals:'
+    @rentals.each do |rental|
         if rental.person.find_id == id.to_i
           puts "Data #{rental.date}, Book #{rental.book.title} by #{rental.book.author}"
         end
       end
-    end
   end
 end
