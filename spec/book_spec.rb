9ed98feaa0873af3145876d4bb0602574bb0f53e
BonkeGcobo <madixmicro@gmail.com>
2022-05-26 19:15:52 +0200

    add_rental method works

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,11 +1,17 @@
 require './book'
-
+require './person'
 describe Book do
   context "Check if book work as it should" do
+    book = Book.new("Born a Crime", "Trevor")
     it "Check if the Book is being initialized" do
-      book = Book.new("Born a Crime", "Trevor")
       author = book.author
       expect(author).to eq("Trevor")
+    end
+
+    it "Test if rental can be added" do
+      person = Person.new("14","Bonke")
+      book.add_rental(person,"2020")
+      expect(book.rentals.length) == 1
     end
   end    
 end
