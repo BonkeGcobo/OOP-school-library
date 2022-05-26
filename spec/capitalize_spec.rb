7c51d3f6b217e7b254ec20989f4a96e8d317a490
BonkeGcobo <madixmicro@gmail.com>
2022-05-26 22:10:19 +0200

    add capitalize

---
+++ spec/capitalize_spec.rb
@@ -0,0 +1,12 @@
+require './capitalize'
+require './student'
+
+describe 'capitalize name' do
+  context 'test capitalize name' do
+    it 'should expect to capitalize name' do
+      student = Student.new(15,nil,'bonke')
+      decorated_student = CapitalizeDecorator.new(student)
+      expect(decorated_student.correct_name).to eq('Bonke')
+    end
+  end
+end
