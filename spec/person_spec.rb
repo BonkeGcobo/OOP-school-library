397e5bbde2232f399abf2d787053b6410db0b8b3
BonkeGcobo <madixmicro@gmail.com>
2022-05-26 19:36:15 +0200

    test person initialization

---
+++ spec/person_spec.rb
@@ -0,0 +1,12 @@
+require './person'
+
+describe Person do
+  context 'Test initialization' do
+    it 'gets initialize only with age' do
+      person = Person.new(14, "Bonke")
+      expect(person.age).to be 14
+      expect(person.name).to eq 'Bonke'
+      expect(person.find_id).to be > 0
+    end
+  end
+end
